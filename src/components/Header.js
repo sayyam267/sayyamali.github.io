e9109a8d40fa0a946e2ce179c0e88e09660cdce2
Sayyam Ali <59091997+sayyam267@users.noreply.github.com>
2022-07-21 02:52:38 +0500

    updated code

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,73 +3,76 @@ import React from 'react'
 import image from '../images/dp.jpg'
 import name from '../audio/name.mp3'
 
-const Header = (props) => (
-  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
-    <div>
-      <img className="logo" src={image} />
-      {/* <span className="icon fa-diamond"></span> */}
-    </div>
-    <div className="content">
-      <div className="inner">
-        <h1>
-          Sayyam Ali{' '}
-          <span
-            onClick={() => name.play()}
-            className="icon fa-volume-up"
-            style={{ cursor: 'pointer' }}
-          />
-        </h1>
-        <p style={{ marginLeft: '21vw', marginRight: '21vw' }}>
-          Software Engineer{'      '}
-          {/* <a href="https://html5up.net">HTML5 UP</a> and released
+const Header = (props) => {
+  const audio = typeof Audio !== 'undefined' && new Audio(name)
+  return (
+    <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+      <div>
+        <img className="logo" src={image} />
+        {/* <span className="icon fa-diamond"></span> */}
+      </div>
+      <div className="content">
+        <div className="inner">
+          <h1>
+            Sayyam Ali{' '}
+            <span
+              onClick={() => audio.play()}
+              className="icon fa-volume-up"
+              style={{ cursor: 'pointer' }}
+            />
+          </h1>
+          <p style={{ marginLeft: '21vw', marginRight: '21vw' }}>
+            Software Engineer{'      '}
+            {/* <a href="https://html5up.net">HTML5 UP</a> and released
           <br />
           for free under the{' '}
           <a href="https://html5up.net/license">Creative Commons</a> license. */}
-        </p>
+          </p>
+        </div>
       </div>
-    </div>
-    <nav>
-      <ul>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('intro')
-            }}
-          >
-            Intro
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('work')
-            }}
-          >
-            Work
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('about')
-            }}
-          >
-            About
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('contact')
-            }}
-          >
-            Contact
-          </button>
-        </li>
-      </ul>
-    </nav>
-  </header>
-)
+      <nav>
+        <ul>
+          <li>
+            <button
+              onClick={() => {
+                props.onOpenArticle('intro')
+              }}
+            >
+              Intro
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => {
+                props.onOpenArticle('work')
+              }}
+            >
+              Work
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => {
+                props.onOpenArticle('about')
+              }}
+            >
+              About
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => {
+                props.onOpenArticle('contact')
+              }}
+            >
+              Contact
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
 
 Header.propTypes = {
   onOpenArticle: PropTypes.func,
